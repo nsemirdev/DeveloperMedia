53e149612b48d7a260380e1bfd5948524fe08dae
nsemirdev <nsemirdev@gmail.com>
2022-12-29 11:42:31 +0300

    configure folder structure

---
+++ DeveloperMedia/App Delegate/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  DeveloperMedia
+//
+//  Created by Emir Alkal on 29.12.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+     
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = SignUpVC()
+    }
+}
+
---
+++ DeveloperMedia/Scenes/Auth/SignUpVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DeveloperMedia
+//
+//  Created by Emir Alkal on 29.12.2022.
+//
+
+import UIKit
+
+class SignUpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemTeal
+    }
+
+
+}
+
